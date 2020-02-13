ed2979b147be335cb5ede98a57018c3db05b88fb
workPC <asifgeass@gmail.com>
2020-02-13 20:46:23 +0300

    123213

--- CancellationTokenSource/MainWindow.xaml.cs
+++ CancellationTokenSource/MainWindow.xaml.cs
@@ -22,8 +22,9 @@ namespace CancellationTokenSourceProjectTest
     /// </summary>
     public partial class MainWindow : Window
     {
-        private CancellationTokenSource cts = null;
+        private CancellationTokenSource cts = new CancellationTokenSource();
         private int count = 0;
+        private Task single;
         public MainWindow()
         {
             InitializeComponent();
@@ -33,29 +34,40 @@ namespace CancellationTokenSourceProjectTest
         {
             Ex.Log($"AddTask_Click()");
             Ex.Try(false,()=>cts.Cancel());
-            cts = null;
             cts = new CancellationTokenSource();
+            WorkTask(count, cts.Token);
             count++;
-            WorkTask(count, cts.Token);            
+            
         }
 
         private void CancelTask_Click(object sender, RoutedEventArgs e)
         {
             Ex.Log($"CancelTask_Click()");
+            Ex.Try(false, () => cts.Cancel());
+            cts = new CancellationTokenSource();
         }
 
         private async Task WorkTask(int name, CancellationToken cancel)
         {
-            while(true)
+            Ex.Log($"WorkTask() {name}");            
+            await Ex.Try(Task.Delay(5000, cancel));
+            if (cancel.IsCancellationRequested)
             {
-                if (cancel.IsCancellationRequested)
-                {
-                    Ex.Log($"{name} CANCELED");
-                    break;
-                }
-                await Task.Delay(1000);
-                Ex.Log($"{name} is working....");
+                Ex.Log($"{name} CANCELED!");
+                return;
             }
+            Ex.Log($"{name} is work done!");
+        }
+
+        private void SingletonTask_Click(object sender, RoutedEventArgs e)
+        {
+            Ex.Log($"SingletonTask_Click()");
+            if (single == null || single.IsCompleted)
+            {
+                count++;
+                single = WorkTask(count, cts.Token);                
+            }
+            else Ex.Log($"{single.Status} {count}");
         }
     }
 }
